dd6ca8b7a820e51ec5a5ca6f1aebe4213c5eb0b3
ANDRII STO <100497629+AndriiStol@users.noreply.github.com>
2024-09-24 17:42:52 +0300

    Update faalingblock.cs

--- faalingblock.cs
+++ faalingblock.cs
@@ -3,13 +3,13 @@ using System.Collections;
 
 public class faalingblock : MonoBehaviour
 {
-    public float bounceForce = 10f; // Сила прыжка ловушки
-    public float detectRadius = 1.5f; // Радиус обнаружения игрока
-    public float jumpCooldown = 2f; // Задержка между прыжками
-    public LayerMask playerLayer; // Слой, на котором находятся игроки
+    public float bounceForce = 10f; 
+    public float detectRadius = 1.5f; 
+    public float jumpCooldown = 2f; 
+    public LayerMask playerLayer; 
 
-    private bool isGrounded = true; // Флаг для отслеживания состояния на земле
-    private bool canJump = true; // Флаг для проверки возможности прыжка
+    private bool isGrounded = true; 
+    private bool canJump = true; 
     private Rigidbody2D rb;
 
     private void Awake()
@@ -21,12 +21,12 @@ public class faalingblock : MonoBehaviour
     {
         if (canJump)
         {
-            // Проверяем наличие игроков в радиусе обнаружения
+           
             Collider2D playerCollider = Physics2D.OverlapCircle(transform.position, detectRadius, playerLayer);
 
             if (playerCollider != null)
             {
-                // Применяем силу прыжка, если ловушка на земле
+               
                 if (isGrounded)
                 {
                     rb.AddForce(Vector2.up * bounceForce, ForceMode2D.Impulse);
@@ -40,7 +40,7 @@ public class faalingblock : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        // Проверяем, столкнулась ли ловушка с землей
+        
         if (collision.gameObject.name == ("Terrain"))
         {
             isGrounded = true;
