5e11beee1cefc818a2cc3adaf12264d5fd95971c
ANDRII STO <100497629+AndriiStol@users.noreply.github.com>
2024-09-24 17:41:22 +0300

    Update LevelController.cs

--- LevelController.cs
+++ LevelController.cs
@@ -3,20 +3,19 @@ using UnityEngine.UI;
 
 public class LevelController : MonoBehaviour
 {
-    public Joystick joystick; // Перетащите ваш Joystick в это поле
-    public Button jumpButton; // Перетащите вашу кнопку прыжка в это поле
+    public Joystick joystick; 
+    public Button jumpButton; 
 
     private void Start()
     {
-        // В начале уровня включаем Joystick и кнопку прыжка
+        
         joystick.gameObject.SetActive(true);
         jumpButton.gameObject.SetActive(true);
     }
 
     public void OnLevelComplete()
     {
-        // По вызову этой функции после завершения уровня
-        // отключаем Joystick и кнопку прыжка
+
         joystick.gameObject.SetActive(false);
         jumpButton.gameObject.SetActive(false);
     }
